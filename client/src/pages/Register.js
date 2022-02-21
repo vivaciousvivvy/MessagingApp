e16656357e02bbfee46255f23f3147f860248d25
Vivek <vivek.srirama@gmail.com>
2022-02-21 14:08:39 -0800

    added form to register page and changed styles

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <section>
+      <h3>Create An Account</h3>
+      <form className='form'>
+        <div className='input_container'>
+          <label htmlFor='name'>Name</label>
+          <input type='text' name='name' />
+        </div>
+        <div className='input_container'>
+          <label htmlFor='email'>Email</label>
+          <input type='text' name='email' />
+        </div>
+        <div className='input_container'>
+          <label htmlFor='password'>Password</label>
+          <input type='password' name='password' />
+        </div>
+      </form>
+    </section>
   )
 }
 
