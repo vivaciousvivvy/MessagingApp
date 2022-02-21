0072ddab3762aa8a60495a26a5aaf43c8627e724
Vivek <vivek.srirama@gmail.com>
2022-02-21 11:12:52 -0800

    Added navbar with links and styled it

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {BrowserRouter, Routes, Router, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    
-    <div>Home</div>
+    <BrowserRouter>
+      <Navbar>
+        <Routes>
+        <Route exact path="/" component={Home} />
+        </Routes>
+      </Navbar>
+      <body>
+        <h1>Home page</h1>
+      </body>
+    </BrowserRouter>
   );
 }
 
