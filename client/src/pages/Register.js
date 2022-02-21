aff4d8900df285d19d5397cb9ec68775d6f00149
Vivek <vivek.srirama@gmail.com>
2022-02-21 14:44:04 -0800

    handle submit and handle change functions added

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,22 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Register = () => {
+
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: null,
+    loading: false,
+  });
+
+  const {name, email, password, error, loading } = data;
+
+  const handleChange = event => {
+    setData({...data, [event.target.name]: event.target.value})
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    console.log(data);
+
+    if(!name || !email || !password) {
+      setData({...data, error: 'All fields are required!'});
+    }
+  };
+
   return (
     <section>
       <h3>Create An Account</h3>
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
         <div className='input_container'>
           <label htmlFor='name'>Name</label>
-          <input type='text' name='name' />
+          <input type='text' name='name' value={name} onChange={handleChange}/>
         </div>
         <div className='input_container'>
           <label htmlFor='email'>Email</label>
-          <input type='text' name='email' />
+          <input type='text' name='email' value={email} onChange={handleChange}/>
         </div>
         <div className='input_container'>
           <label htmlFor='password'>Password</label>
-          <input type='password' name='password' />
+          <input type='password' name='password' value={password} onChange={handleChange}/>
         </div>
+        {error ? <p className='error'>{error}</p> : null}
         <div className='btn_container'>
           <button className='btn'>Register</button>
         </div>
