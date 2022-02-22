54c065f469d33b2a6b48ebf8f026388171656175
Vivek <vivek.srirama@gmail.com>
2022-02-21 17:41:54 -0800

    added authstate listener to Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,20 @@ import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import Login from './pages/Login';
-
+import AuthProvider from './context/auth';
 
 function App() {
   return (
-    <BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
       <Navbar />
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { auth, db } from '../firebase'
 import { signOut } from 'firebase/auth'
 import { updateDoc, doc } from 'firebase/firestore'
+import { AuthContext } from '../context/auth'
 
 const Navbar = () => {
 
+  const { user } = useContext(AuthContext);
   const handleSignout = async () => {
     await updateDoc(doc(db, 'users', auth.currentUser.uid), {
       isOnline: false,
@@ -19,7 +21,7 @@ const Navbar = () => {
             <Link to="/">Messenger</Link>
         </h3>
         <div>
-          {auth.currentUser ? (
+          {user ? (
           <>
             <Link to='/profile'>Profile</Link>
             <button className='btn' onClick={handleSignout}>Logout</button>
