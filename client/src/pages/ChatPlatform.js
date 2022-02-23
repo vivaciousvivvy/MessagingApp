2e821ad32ca5812f4657cdb8f772e63ed72998c1
Vivek <vivek.srirama@gmail.com>
2022-02-23 09:44:03 -0800

    grabbed username and roomid variables and passed it to socket

--- client/src/pages/ChatPlatform.js
+++ client/src/pages/ChatPlatform.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import io from 'socket.io-client'
 import ChatRoomHeader from '../components/ChatRoomHeader'
 import MessageInput from '../components/MessageInput'
 import Messages from '../components/Messages'
 
+let socket;
+
 const ChatPlatform = () => {
+  const [userName, setUserName] = useState('');
+  const [roomId, setRoomId] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const {username, roomId } = params;
+    socket = io('http://localhost:5000');
+    setUserName(userName);
+    setRoomId(roomId);
+
+    socket.emit('join', { userName, roomId }, (error) => {
+      if(error) 
+        alert(error);
+    })
+  }, [params])
+  
+
   return (
     <section>
       <ChatRoomHeader />
