f94c032eaf7781fbf7f17e79d8cf2c63418bc816
Vivek <vivek.srirama@gmail.com>
2022-02-23 10:23:09 -0800

    added user functions

---
+++ server/users.js
@@ -0,0 +1,48 @@
+
+const users = [];
+
+const addNewUser = ({id, userName, roomId}) => {
+    userName = userName.trim().toLowerCase();
+    roomId = roomId.trim().toLowerCase();
+
+    if(!userName || !roomId) {
+        return {
+            error: 'Useranme and room id are required!',
+        }
+    }
+
+    const existsUser = users.find(
+        (user) => user.roomId === roomId && user.userName === userName
+    );
+    
+    if(existsUser) {
+        return {
+            error: 'Username is already taken!!',
+        }
+    }
+    
+    users.push({id, username, roomId});
+    
+    
+    return { users };
+}
+
+
+const removeExistUser = (id) => {
+    const index = users.findIndex((user) => user.id === id);
+
+    if(index !== -1) {
+        return users.splice(index, 1)[0];
+    }
+}
+
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+
+const getUsersInARoom = (roomId) => {
+    roomId = roomId.trim.toLowerCase();
+    return users.filter((user) => user.roomId === roomId);
+}
+
+module.exports = {addNewUser, removeExistUser, getUser, getUsersInARoom};
