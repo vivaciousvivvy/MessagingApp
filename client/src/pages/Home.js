023c6d685571f84bcce296ab3bf9808928bc3b12
Vivek <vivek.srirama@gmail.com>
2022-02-22 21:53:59 -0800

    Created form to let users join a specific chat room

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home Page</div>
+    <div className='content-join-form'>
+      <label>Enter Your Nick Name : </label>
+      <input type='text' placeholder='Nick Name' className='inputBox' />
+      <br/>
+      <br/>
+      <label>Enter Room ID : </label>
+      <input type='text' className='input' />
+      <Link to='/chatting/:roomId/:userName'>
+        <button className='btn-join'>Join</button>
+      </Link>
+    </div>
   )
 }
 
