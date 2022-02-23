4ed813d84857634524a19890af4711d01edc8155
Vivek <vivek.srirama@gmail.com>
2022-02-23 11:52:03 -0800

    scrolls to bottom when a new message is sent

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { useRef } from 'react'
 import Message from './Message'
+import { useEffect } from 'react'
 
 const Messages = ({ messages, userName }) => {
+  const messagesEndRef = useRef(null);
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  useEffect(scrollToBottom, [messages]);
+  
+
+
   return (
     <main className='msger-chat'>
       {messages.map((message, i) => (
