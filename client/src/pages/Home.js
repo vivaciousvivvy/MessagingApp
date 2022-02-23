f840a2c868cb90f9633185e574066917aff261f7
Vivek <vivek.srirama@gmail.com>
2022-02-23 08:38:04 -0800

    added link to join button and read inputs from user

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
+  const [userName, setUserName] = useState('');
+  const[roomId, setRoomId] = useState('');
   return (
     <div className='content-join-form'>
       <label>Enter Your Nick Name : </label>
-      <input type='text' placeholder='Nick Name' className='inputBox' />
+      <input type='text' placeholder='Nick Name' className='inputBox' onChange={(event) => setUserName(event.target.value)} />
       <br/>
       <br/>
       <label>Enter Room ID : </label>
-      <input type='text' className='input' />
-      <Link to='/chatting/:roomId/:userName'>
+      <input type='text' className='input' onChange={(event) => setRoomId(event.target.value)}/>
+      <Link to={userName && roomId ? `chatting/${roomId}/${userName}` : ''}>
         <button className='btn-join'>Join</button>
       </Link>
     </div>
