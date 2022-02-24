49b2f6391850031c6dcea406b008a2c43ed9c7b1
Vivek <vivek.srirama@gmail.com>
2022-02-23 17:20:20 -0800

    removed unused import

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Routes, Router, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Register from './pages/Register';
