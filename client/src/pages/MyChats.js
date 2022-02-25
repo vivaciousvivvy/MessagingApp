37155c1b2b050f20e33769a55bb4cf5988838b10
Vivek <vivek.srirama@gmail.com>
2022-02-25 13:55:27 -0800

    figuring out how to query db

--- client/src/pages/MyChats.js
+++ client/src/pages/MyChats.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import { doc, getDoc, } from "firebase/firestore";
+import { db, auth } from '../firebase';
 
 const MyChats = () => {
-  return (
-      <div>
-          <li>MyChats</li>
-      </div>
-  )
+    let chatroutes = [];
+    
+    //Find out how to query
+    //Cannot access docSnap outside of docSnap getDoc function
+    async function getChats() {
+      const docRef = doc(db, "users' chats", auth.currentUser.uid);
+      const docSnap = await getDoc(docRef);
+      if(docSnap.exists())
+      {
+        console.log("Routes!!!!" + docSnap.get("routes"));
+        return docSnap.data();
+      }
+      return [];
+    }
+
+    chatroutes = getChats();
+
+    /*
+    getDoc(doc(db, "users' chats", auth.currentUser.uid)).then(docSnap => {
+        if (docSnap.exists()) {
+          chatroutes = docSnap.get("routes");
+          console.log("The chat routes are " + chatroutes);
+        } else {
+          console.log("No such document!");
+        }
+      })
+      */
+
+      //Console.log statement displaying empty array
+      console.log("These are the chat routes!!" + chatroutes);
+
+    return (
+        <div>
+            <li>{auth.currentUser.uid}</li>
+        </div>
+    )
 }
 
 export default MyChats
