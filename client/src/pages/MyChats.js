a067425bb6caa940b330fc070283ef2120abbd60
Vivek <vivek.srirama@gmail.com>
2022-02-25 20:08:45 -0800

    Pulls chat routes from db successfully

--- client/src/pages/MyChats.js
+++ client/src/pages/MyChats.js
@@ -1,42 +1,28 @@
-import React from 'react'
-import { doc, getDoc, } from "firebase/firestore";
+import React, { useState, useEffect } from 'react'
+import { getDoc, doc } from "firebase/firestore";
 import { db, auth } from '../firebase';
 
 const MyChats = () => {
-    let chatroutes = [];
-    
-    //Find out how to query
-    //Cannot access docSnap outside of docSnap getDoc function
-    async function getChats() {
-      const docRef = doc(db, "users' chats", auth.currentUser.uid);
-      const docSnap = await getDoc(docRef);
-      if(docSnap.exists())
-      {
-        console.log("Routes!!!!" + docSnap.get("routes"));
-        return docSnap.data();
-      }
-      return [];
-    }
+    const [routes, setRoutes] = useState([]);
 
-    chatroutes = getChats();
-
-    /*
-    getDoc(doc(db, "users' chats", auth.currentUser.uid)).then(docSnap => {
+    useEffect(() => {
+      //WORKING FUNCTION DO NOT DELETE!!!!!
+      getDoc(doc(db, "users' chats", auth.currentUser.uid)).then(docSnap => {
         if (docSnap.exists()) {
-          chatroutes = docSnap.get("routes");
-          console.log("The chat routes are " + chatroutes);
+          let currentRoutes = [];
+          currentRoutes = docSnap.get("routes");
+          console.log("The chat routes are " + currentRoutes);
+          setRoutes(currentRoutes);
         } else {
           console.log("No such document!");
         }
       })
-      */
-
-      //Console.log statement displaying empty array
-      console.log("These are the chat routes!!" + chatroutes);
-
+    }, [])
+    console.log("Outside the function!!!!" + routes);
+  
     return (
         <div>
-            <li>{auth.currentUser.uid}</li>
+            <li>Chat Rooms Here</li>
         </div>
     )
 }
